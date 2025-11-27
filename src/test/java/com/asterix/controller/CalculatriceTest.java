89fadac7766588bf72178e696af3400da56e9494
b24015232 <heather.BURBECK@etu.univ-amu.fr>
2025-11-27 10:08:13 +0100

    test(Calculatrice): Creation de la classe calculatrice et de son test afin de tester l'efficacité de JaCoCo

---
+++ src/test/java/com/asterix/controller/CalculatriceTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatriceTest {
+
+    @Test
+    public void testAddition() {
+        Calculatrice calc = new Calculatrice();
+        // Ce test va exécuter la méthode additionner, donc JaCoCo va la marquer comme "vue"
+        assertEquals(5, calc.additionner(2, 3));
+    }
+}
