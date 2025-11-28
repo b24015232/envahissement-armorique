1da918a54ba0cc47b40e92451252934369e58a2b
ARSENA-Claire-2326008b <claire.arsena@etu.univ-amu.fr>
2025-11-28 10:22:25 +0100

    feat(Gaul):ajout d'un fragment de code de la classe fille gaul

--- src/main/java/com/asterix/model/character/gaul/Gaul.java
+++ src/main/java/com/asterix/model/character/gaul/Gaul.java
@@ -1,4 +1,15 @@
 package com.asterix.model.character.gaul;
 
-public class Gaul {
+import com.asterix.model.character.Character;
+
+public class Gaul extends Character {
+
+    public Gaul(String name, int health) {
+        super(name, health); // appelle le constructeur mère
+    }
+
+    // juste pour tester le workflow
+    public String parler() {
+        return "bonjour";
+    }
 }
