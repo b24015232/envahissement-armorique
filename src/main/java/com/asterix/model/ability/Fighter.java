f23231a9b91f741d57c8b5c385edcd011556a446
ARSENA-Claire-2326008b <claire.arsena@etu.univ-amu.fr>
2025-12-05 16:28:21 +0100

    feat(Fighter):adding the worker interface

--- src/main/java/com/asterix/model/ability/Fighter.java
+++ src/main/java/com/asterix/model/ability/Fighter.java
@@ -1,4 +1,16 @@
 package com.asterix.model.ability;
 
+import com.asterix.model.character.Character;
+
+/**
+ * Interface defining the capability to fight
+ * Used by soldiers, druids, and creatures
+ */
 public interface Fighter {
+
+    /**
+     * Engages in combat with an opponent
+     * @param opponent the character to fight against
+     */
+    void fight(Character opponent);
 }
