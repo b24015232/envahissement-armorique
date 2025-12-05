c0f53cf42bab59ae10cc7a7c2cd9881fb3215497
ARSENA-Claire-2326008b <claire.arsena@etu.univ-amu.fr>
2025-12-05 14:26:44 +0100

    feat(gaul):implemening basic gaul class

--- src/main/java/com/asterix/model/character/gaul/Gaul.java
+++ src/main/java/com/asterix/model/character/gaul/Gaul.java
@@ -1,15 +1,30 @@
 package com.asterix.model.character.gaul;
 
 import com.asterix.model.character.Character;
+import com.asterix.model.character.Gender;
 
+/**
+ * Represents a gaul character
+ * Specificity : can drink magic potion
+ */
 public class Gaul extends Character {
 
-    public Gaul(String name, int health) {
-        super(name, health); // appelle le constructeur mère
+    public Gaul(String name, int age, double height, double strength, double stamina, Gender gender) {
+        super(name, age, height, strength, stamina, gender);
     }
 
-    // juste pour tester le workflow
-    public String parler() {
-        return "bonjour";
+    /**
+     * Consumes a dose of magic potion
+     * Increases the potion level attribute
+     * @param dose Quantity of potion drunk
+     */
+    public void drinkPotion(double dose) {
+        this.potionLevel += dose;
+        System.out.println(this.name + " drinks " + dose + " of magic potion!");
+    }
+
+    @Override
+    public String toString() {
+        return "Gaul [Name=" + name + ", Health=" + health + ", Strength=" + strength + "]";
     }
 }
