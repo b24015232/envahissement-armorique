dfd2ae555cdd2eeae7ca9ab53eeff5933203dcf8
ARSENA-Claire-2326008b <claire.arsena@etu.univ-amu.fr>
2025-12-05 16:28:35 +0100

    feat(Leader):adding the leader interface

--- src/main/java/com/asterix/model/ability/Leader.java
+++ src/main/java/com/asterix/model/ability/Leader.java
@@ -1,4 +1,13 @@
 package com.asterix.model.ability;
 
+/**
+ * Interface defining the capability to lead others.
+ * Used by chiefs, generals, and druids (spiritual leaders)
+ */
 public interface Leader {
+
+    /**
+     * Issues commands or guidance to others
+     */
+    void command();
 }
