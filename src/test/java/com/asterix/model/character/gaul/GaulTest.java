4075db753afacf51ea82bcd796d2768d5c25867d
ARSENA-Claire-2326008b <claire.arsena@etu.univ-amu.fr>
2025-12-05 14:33:52 +0100

    test(GaulTest):testing the start of gaul class

--- src/test/java/com/asterix/model/character/gaul/GaulTest.java
+++ src/test/java/com/asterix/model/character/gaul/GaulTest.java
@@ -1,25 +1,39 @@
 package com.asterix.model.character.gaul;
 
+import com.asterix.model.character.Gender;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class GaulTest {
+/**
+ * Unit tests for the gaul class
+ * Also validates the logic of the abstract character class
+ */
+public class GaulTest {
 
     @Test
-    void unGauloisDoitAvoirUnNom() {
-        // création d'un gaulois
-        Gaul asterix = new Gaul("Astérix", 100);
+    public void testGaulInitialization() {
+        // creating Casalix's gaul version
+        String name = "Casalix";
+        int age = 40;
+        double height = 1.75;
+        double strength = 69.0;
+        double stamina = 100.0;
+        Gender gender = Gender.MALE;
 
-        // vérification de la compatibilité avec la classe mère
-        assertEquals("Astérix", asterix.getName());
+        Gaul asterix = new Gaul(name, age, height, strength, stamina, gender);
+
+        // assert check heritage
+        assertEquals("Casalix", asterix.getName(), "Name should be initialized correctly");
+        assertEquals(100.0, asterix.getHealth(), "Health should default to 100.0");
+        assertNotNull(asterix.toString(), "ToString should not return null");
     }
 
     @Test
-    void unGauloisDoitSavoirParler() {
-        // création d'un gaulois
-        Gaul obelix = new Gaul("Obélix", 150);
+    public void testDrinkPotion() {
+        // Arrange
+        Gaul obelix = new Gaul("Balerdix", 26, 1.87, 150.0, 200.0, Gender.MALE);
 
-        // vérification de la méthode
-        assertEquals("bonjour", obelix.parler());
+        // Act
+        obelix.drinkPotion(10.0);
     }
 }
