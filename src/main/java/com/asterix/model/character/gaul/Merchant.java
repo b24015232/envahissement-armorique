4da9a4c232ec259d2003b5ef2b44bf150908f40e
ARSENA-Claire-2326008b <claire.arsena@etu.univ-amu.fr>
2025-12-06 16:44:50 +0100

    feat(merchant):implementing merchant class

--- src/main/java/com/asterix/model/character/gaul/Merchant.java
+++ src/main/java/com/asterix/model/character/gaul/Merchant.java
@@ -1,4 +1,22 @@
 package com.asterix.model.character.gaul;
 
-public class Merchant {
+import com.asterix.model.ability.Worker;
+import com.asterix.model.character.Gender;
+
+public class Merchant extends Gaul implements Worker {
+
+    public Merchant(String name, int age, double height, double strength, double stamina, Gender gender) {
+        super(name, age, height, strength, stamina, gender);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(this.getName() + " sets up his stall at the village market");
+        System.out.println("\"Fresh fish ! My fish is fucking fresh ! Fresh from the Vieux-Port !\"");
+    }
+
+    @Override
+    public String toString() {
+        return "Merchant " + super.toString();
+    }
 }
