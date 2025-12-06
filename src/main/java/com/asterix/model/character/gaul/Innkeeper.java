566e733d5ae80ecac432ead34153fa9647da4528
ARSENA-Claire-2326008b <claire.arsena@etu.univ-amu.fr>
2025-12-06 16:31:12 +0100

    feat(innkeeper):implementing innkeeper class

--- src/main/java/com/asterix/model/character/gaul/Innkeeper.java
+++ src/main/java/com/asterix/model/character/gaul/Innkeeper.java
@@ -1,4 +1,22 @@
 package com.asterix.model.character.gaul;
 
-public class Innkeeper {
+import com.asterix.model.ability.Worker;
+import com.asterix.model.character.Gender;
+
+public class Innkeeper extends Gaul implements Worker {
+
+    public Innkeeper(String name, int age, double height, double strength, double stamina, Gender gender) {
+        super(name, age, height, strength, stamina, gender);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(this.getName() + " cleans the counter and brings out the mugs");
+            System.out.println("\"Who wants beer and wildboar ?\"");
+    }
+
+    @Override
+    public String toString() {
+        return "Innkeeper " + super.toString();
+    }
 }
