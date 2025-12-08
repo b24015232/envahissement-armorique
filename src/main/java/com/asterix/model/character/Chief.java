351b4f62d50361531b4ed3c5a634871fc91a15e7
heather <burbeckheather@gmail.com>
2025-12-08 16:15:58 +0100

    feat: create Chief class

---
+++ src/main/java/com/asterix/model/character/Chief.java
@@ -0,0 +1,13 @@
+package com.asterix.model.character;
+
+public class Chief {
+    private String name;
+    public Chief(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {}
+}
