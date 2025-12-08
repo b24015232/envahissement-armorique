1adc338b176a0eb4fe8265fff0a2eb1d95867d60
heather <burbeckheather@gmail.com>
2025-12-08 16:10:12 +0100

    feat: implement place package logic

--- src/main/java/com/asterix/model/place/Battlefield.java
+++ src/main/java/com/asterix/model/place/Battlefield.java
@@ -1,4 +1,34 @@
 package com.asterix.model.place;
 
-public class Battlefield  {
+import com.asterix.model.character.Character;
+
+/**
+ * Represents a battlefield where combats mainly occur.
+ * <p>
+ * A battlefield is a neutral ground that can contain any type of character.
+ * It is not managed by a specific chief.
+ * </p>
+ *
+ * [cite_start]Reference: [cite: 504, 536]
+ */
+public final class Battlefield extends Place {
+
+    public Battlefield(String name, double area) {
+        super(name, area);
+    }
+
+    /**
+     * Validates entry rules for a battlefield.
+     * <p>
+     * A battlefield can contain all types of characters.
+     * </p>
+     *
+     * @param c The character.
+     * @return Always {@code true}.
+     * [cite_start]Reference: [cite: 504]
+     */
+    @Override
+    protected boolean canEnter(Character c) {
+        return true;
+    }
 }
