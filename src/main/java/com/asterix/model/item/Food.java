d8240d9eff9650cbfa84b91ab28ce3b5fe458b44
ARSENA-Claire-2326008b <claire.arsena@etu.univ-amu.fr>
2025-12-11 13:19:49 +0100

    feat(Food):adding foodtype + isFresh method

--- src/main/java/com/asterix/model/item/Food.java
+++ src/main/java/com/asterix/model/item/Food.java
@@ -3,95 +3,47 @@ package com.asterix.model.item;
 /**
  * Represents a generic food item in the simulation.
  * <p>
- * A {@code Food} has a name and a type (for example "MEAT", "VEGETABLE", "MAGIC"...)
- * and provides an abstract method {@link #getScore()} that defines
- * how many points this food gives (or removes) when consumed or used.
- * It also stores information about which kinds of characters
- * are allowed to eat it (Gauls and/or Romans).
+ * A {@code Food} is defined by its {@link FoodType} which provides its base properties.
+ * It provides an abstract method {@link #getScore()} and a {@link #isFresh()} check
+ * crucial for potion brewing.
  * </p>
  */
 public abstract class Food {
 
-    /**
-     * Display name of the food item.
-     */
     protected final String name;
-
-    /**
-     * Logical type/category of the food (e.g. "MEAT", "DRINK", "MAGIC").
-     */
     protected final String type;
-
-    /**
-     * Indicates whether this food can be eaten by Gauls.
-     */
     protected final boolean gaulEdible;
-
-    /**
-     * Indicates whether this food can be eaten by Romans.
-     */
     protected final boolean romanEdible;
+    //correcting : adding a link to enum foodType
+    protected final FoodType foodType;
 
     /**
-     * Creates a new {@code Food} with the given name, type and eating permissions.
-     *
-     * @param name        the display name of the food
-     * @param type        the logical type/category of the food
-     * @param gaulEdible  {@code true} if Gauls are allowed to eat this food
-     * @param romanEdible {@code true} if Romans are allowed to eat this food
+     * Creates a new {@code Food} based on its Enum definition.
+     * @param foodType The enum constant defining this food's properties.
      */
-    protected Food(String name, String type, boolean gaulEdible, boolean romanEdible) {
-        this.name = name;
-        this.type = type;
-        this.gaulEdible = gaulEdible;
-        this.romanEdible = romanEdible;
+    protected Food(FoodType foodType) {
+        this.foodType = foodType;
+        this.name = foodType.getName();
+        this.type = foodType.getType();
+        this.gaulEdible = foodType.isGaulCanEat();
+        this.romanEdible = foodType.isRomanCanEat();
     }
 
-    /**
-     * Returns the display name of this food item.
-     *
-     * @return the name of the food
-     */
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
+    public String getType() { return type; }
+    public boolean canBeEatenByGaul() { return gaulEdible; }
+    public boolean canBeEatenByRoman() { return romanEdible; }
+
+    public FoodType getFoodType() { return foodType; }
 
     /**
-     * Returns the logical type/category of this food item.
-     *
-     * @return the type of the food (e.g. "MEAT", "DRINK")
+     * Checks if the food is currently fresh.
+     * By default, non-perishable food is always considered fresh.
+     * @return true if fresh.
      */
-    public String getType() {
-        return type;
+    public boolean isFresh() {
+        return true;
     }
 
-    /**
-     * Indicates whether a Gaul is allowed to eat this food.
-     *
-     * @return {@code true} if a Gaul can eat this food, {@code false} otherwise
-     */
-    public boolean canBeEatenByGaul() {
-        return gaulEdible;
-    }
-
-    /**
-     * Indicates whether a Roman is allowed to eat this food.
-     *
-     * @return {@code true} if a Roman can eat this food, {@code false} otherwise
-     */
-    public boolean canBeEatenByRoman() {
-        return romanEdible;
-    }
-
-    /**
-     * Returns the score associated with this food.
-     * <p>
-     * The score usually represents how beneficial or harmful the food is
-     * when used in the simulation (for example, when eaten by a character
-     * or used as an ingredient in a potion).
-     * </p>
-     *
-     * @return the score value for this food
-     */
     public abstract int getScore();
 }
