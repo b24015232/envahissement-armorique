bec83b653319371f099f2bdb15885ceafb231778
ARSENA-Claire-2326008b <claire.arsena@etu.univ-amu.fr>
2025-12-11 13:11:38 +0100

    test(SimpleFood):testing the use of enum type instead manual

--- src/test/java/com/asterix/model/item/SimpleFoodTest.java
+++ src/test/java/com/asterix/model/item/SimpleFoodTest.java
@@ -1,37 +1,35 @@
 package com.asterix.model.item;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleFoodTest {
 
     @Test
     void simpleFoodShouldKeepNameAndType() {
-        // given
-        SimpleFood food = new SimpleFood("Carrot", "VEGETABLE", 2, true, true);
+        // using a non perishable item (salt)
+        SimpleFood food = new SimpleFood(FoodType.SALT);
 
-        // then
-        assertEquals("Carrot", food.getName());
-        assertEquals("VEGETABLE", food.getType());
+        // Then
+        assertEquals("Salt", food.getName());
+        assertEquals("CONDIMENT", food.getType());
     }
 
     @Test
     void simpleFoodShouldReturnBaseScore() {
-        // given
-        SimpleFood food = new SimpleFood("Honey", "SWEET", 4, true, true);
+        // using rock oil
+        SimpleFood food = new SimpleFood(FoodType.ROCK_OIL);
 
-        // then
-        assertEquals(4, food.getScore());
-        assertEquals(4, food.getBaseScore());
+        // Then
+        assertEquals(-10, food.getScore());
     }
 
     @Test
     void simpleFoodShouldExposeEdibleFlags() {
-        // given
-        SimpleFood food = new SimpleFood("Rock oil", "LIQUID", -10, false, true);
+        // authorized only to romans
+        SimpleFood food = new SimpleFood(FoodType.ROCK_OIL);
 
-        // then
+        // Then
         assertFalse(food.canBeEatenByGaul());
         assertTrue(food.canBeEatenByRoman());
     }
