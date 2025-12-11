4802f3b4493739a27fde6f9927a1d78711cad9a0
ARSENA-Claire-2326008b <claire.arsena@etu.univ-amu.fr>
2025-12-11 19:08:55 +0100

    feat(druid): implement random potion recipes logic

--- src/main/java/com/asterix/model/character/gaul/Druid.java
+++ src/main/java/com/asterix/model/character/gaul/Druid.java
@@ -6,132 +6,107 @@ import com.asterix.model.ability.Worker;
 import com.asterix.model.character.Character;
 import com.asterix.model.character.Gender;
 import com.asterix.model.item.Cauldron;
+import com.asterix.model.item.Food;
 import com.asterix.model.item.FoodType;
 import com.asterix.model.item.PerishableFood;
+import java.util.Random;
 
-/**
- * Represents a Druid in the Gaulish village (e.g., Panoramix).
- * <p>
- * The Druid is a central character with multiple capabilities:
- * he works (gathers mistletoe), fights when necessary, and provides leadership.
- * Most importantly, he is the only one capable of brewing the magic potion.
- * </p>
- * Implements {@link Worker}, {@link Fighter}, and {@link Leader}.
- */
-public class Druid extends Gaul implements Worker, Fighter, Leader {
+public abstract class Druid extends Gaul implements Worker, Fighter, Leader {
 
-    /**
-     * The cauldron currently being used by the Druid.
-     */
     private Cauldron currentCauldron;
+    private final Random random;
 
-    /**
-     * Constructs a new Druid character.
-     *
-     * @param name     The name of the druid.
-     * @param age      The age of the druid.
-     * @param height   The height of the druid in meters.
-     * @param strength The physical strength of the druid.
-     * @param stamina  The stamina/endurance of the druid.
-     * @param gender   The gender of the druid.
-     */
     public Druid(String name, int age, double height, double strength, double stamina, Gender gender) {
         super(name, age, height, strength, stamina, gender);
+        this.random = new Random();
     }
 
-    /**
-     * Prepares a fresh cauldron of magic potion.
-     * <p>
-     * This unique ability involves mixing specific ingredients like mistletoe,
-     * fresh clover, and rock oil (or beet juice).
-     * The Druid must ensure the fish is "average" (partially fresh) for the recipe to succeed.
-     * </p>
-     */
     public void concoctPotion() {
-        System.out.println(this.getName() + " lights a fire under the cauldron and starts brewing...");
-
+        System.out.println(this.getName() + " lights a fire under the cauldron...");
         this.currentCauldron = new Cauldron();
 
-        // 1. Add standard ingredients
+        // basic ingredients
         currentCauldron.addIngredient(FoodType.MISTLETOE.create());
         currentCauldron.addIngredient(FoodType.CARROT.create());
-        currentCauldron.addIngredient(FoodType.SALT.create());
-        currentCauldron.addIngredient(FoodType.HONEY.create());
-        currentCauldron.addIngredient(FoodType.MEAD.create());
-        currentCauldron.addIngredient(FoodType.SECRET_INGREDIENT.create());
 
-        // Substitution allowed: Rock Oil or Beet Juice
-        currentCauldron.addIngredient(FoodType.ROCK_OIL.create());
-
-        // 2. Add freshness-sensitive ingredients
-        // Clover must be FRESH (default state)
-        currentCauldron.addIngredient(FoodType.CLOVER.create());
-
-        // 3. Special Handling: Fish must be "AVERAGE" (Partially Fresh)
-        // We create a fresh fish, then let time pass once to degrade it.
-        PerishableFood fish = (PerishableFood) FoodType.FISH.create();
-        fish.passTime(); // Transitions from Fresh -> PartiallyFresh (Average)
-        System.out.println("The druid waits for the fish to smell... just enough.");
-
+        // fish
+        Food fish = FoodType.FISH.create();
+        if(fish instanceof PerishableFood) ((PerishableFood) fish).passTime();
         currentCauldron.addIngredient(fish);
 
-        // Attempt to brew
-        boolean success = currentCauldron.brew();
+        // random recipe
+        double roll = random.nextDouble();
 
-        if (success) {
-            System.out.println("By Toutatis! The magic potion is ready!");
-        } else {
-            System.out.println("By Belenos... I messed up the recipe.");
+        if (roll < 0.6) {
+            // standard potion
+            currentCauldron.addIngredient(FoodType.ROCK_OIL.create());
+            System.out.println("Standard potion");
         }
+
+        else if (roll < 0.75) {
+            // feeding potion
+            currentCauldron.addIngredient(FoodType.ROCK_OIL.create());
+            currentCauldron.addIngredient(new Food(FoodType.FISH) {
+                @Override public String getName() {
+                    return "Lobster";
+                }
+
+                @Override public int getScore() {
+                    return 10;
+                }
+            });
+            System.out.println("Feeding potion");
+        }
+        else if (roll < 0.90) {
+            // duplication potion
+            currentCauldron.addIngredient(FoodType.ROCK_OIL.create());
+            currentCauldron.addIngredient(new Food(FoodType.MEAD) {
+                @Override public String getName() {
+                    return "Two headed unicorn milk";
+                }
+                @Override public int getScore() {
+                    return 0;
+                }
+            });
+            System.out.println("Duplication potion");
+        }
+        else {
+            // metamorphosis into lycantrhope potion
+            currentCauldron.addIngredient(FoodType.ROCK_OIL.create());
+            currentCauldron.addIngredient(new Food(FoodType.MISTLETOE) {
+                @Override public String getName() {
+                    return "Idefix's hair";
+                }
+
+                @Override public int getScore() {
+                    return 0;
+                }
+            });
+            System.out.println("Metamorphic potion");
+        }
+        this.currentCauldron.brew();
     }
 
-    /**
-     * Serves a dose of magic potion to a fellow Gaul.
-     *
-     * @param gaul The Gaul to receive the potion.
-     */
     public void servePotion(Gaul gaul) {
         if (currentCauldron != null) {
-            double dose = currentCauldron.takeLadle();
-            if (dose > 0) {
-                gaul.drinkPotion(dose);
-            } else {
-                System.out.println("The cauldron is empty!");
-            }
-        } else {
-            System.out.println("There is no cauldron ready yet.");
+            gaul.drinkPotionFromCauldron(currentCauldron);
         }
     }
 
-    // --- Interface Implementations ---
-
-    /**
-     * Implementation of the {@link Worker} interface.
-     */
-    @Override
-    public void work() {
-        System.out.println(this.getName() + " goes into the forest to gather fresh mistletoe with a golden sickle");
+    public Cauldron getCauldron() {
+        return this.currentCauldron;
     }
 
-    /**
-     * Implementation of the {@link Leader} interface.
-     */
-    @Override
-    public void command() {
-        System.out.println(this.getName() + " raises a hand for silence");
+    //some narration
+    @Override public void work() {
+        System.out.println(this.getName() + " gathers mistletoe.");
     }
 
-    /**
-     * Implementation of the {@link Fighter} interface.
-     */
-    @Override
-    public void fight(Character opponent) {
-        System.out.println(this.getName() + " hits " + opponent.getName() + " with his staff!");
+    @Override public void command() {
+        System.out.println(this.getName() + " raises a hand.");
+    }
+
+    @Override public void fight(Character opponent) {
         resolveFight(opponent);
-    }
-
-    @Override
-    public String toString() {
-        return "Druid " + super.toString();
     }
 }
